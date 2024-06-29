9a1c002c27abcb7efa01de276f8f03888b90bb74
Natan-Mesele <huluale12@gmail.com>
2024-06-29 11:28:07 +0300

    chat

--- projectmang/src/Redux/Chat/Action.js
+++ projectmang/src/Redux/Chat/Action.js
@@ -13,7 +13,7 @@ export const sendMessage=(messageData) => {
                 type: actionTypes.SEND_MESSAGE_SUCCESS,
                 message: response.data,
             });
-
+                console.log("message sent", response.data)
         }catch (error){
             console.log(error)
             dispatch({
